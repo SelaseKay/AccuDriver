ade3ef871713d852330bc243f30c4612411a4d49
silvaman47 <aosige47@gmail.com>
2021-07-17 20:15:53 +0000

    video

--- lib/screens/videoscreen.dart
+++ lib/screens/videoscreen.dart
@@ -14,7 +14,7 @@ class _videoscreenState extends State<videoscreen> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network('https://www.youtube.com/watch?v=0SynvJKXgYI')
+    _controller = VideoPlayerController.network('https://drive.google.com/uc?export=download&id=1oVMB2cHzzCWkLgh8ZQxUVGmLnJgECheH')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
@@ -59,4 +59,62 @@ class _videoscreenState extends State<videoscreen> {
     super.dispose();
     _controller.dispose();
   }
+}
+
+class _ButterFlyAssetVideo extends StatefulWidget {
+  @override
+  _ButterFlyAssetVideoState createState() => _ButterFlyAssetVideoState();
+}
+
+class _ButterFlyAssetVideoState extends State<_ButterFlyAssetVideo> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/Butterfly-209.mp4');
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 20.0),
+          ),
+          const Text('With assets mp4'),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  VideoPlayer(_controller),
+                  _ControlsOverlay(controller: _controller),
+                  VideoProgressIndicator(_controller, allowScrubbing: true),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _ControlsOverlay({required VideoPlayerController controller}) {}
 }
